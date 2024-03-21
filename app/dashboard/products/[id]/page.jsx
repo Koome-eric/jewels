2f560f953b95dac7bfdd8c281d9f53ad37a1878d
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-03-21 14:36:46 +0300

    Update page.jsx

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -1,5 +1,5 @@
 import { updateProduct } from "app/lib/actions";
-import { fetchProduct, fetchDonationsForContactByName } from "app/lib/data";
+import { fetchProduct, fetchDonationsForContactByName, getTotalDonatedAmount } from "app/lib/data";
 import styles from "app/ui/dashboard/products/singleProduct/singleProduct.module.css";
 import Image from "next/image";
 
@@ -7,6 +7,7 @@ const SingleProductPage = async ({ params }) => {
   const { id } = params;
   const product = await fetchProduct(id);
   const donations = await fetchDonationsForContactByName(product.name); // Fetch donations for this contact by name
+  const totalDonatedAmount = await getTotalDonatedAmount(product.name); // Get total donated amount for this contact
 
   return (
     <div className={styles.container}>
@@ -15,6 +16,9 @@ const SingleProductPage = async ({ params }) => {
           <Image src="/noavatar.png" alt="" width={60} height={60} />
         </div>
         <h1>{product.name}</h1> {/* Display the contact's name */}
+        <div className={styles.productInfo}>
+        <h2>Total Donated: <span>${totalDonatedAmount.toFixed(2)}</span></h2>
+      </div>
       </div>
       
       <div className={styles.donationsContainer}>
